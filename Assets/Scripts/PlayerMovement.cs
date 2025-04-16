87f8a9048ea8befc742e6a2fc2036e244a29b5d7
9ot9am <9ot9am@gmail.com>
2025-04-16 17:33:45 +0900

    feat(player) : Jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,19 +1,26 @@
+using System.Numerics;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Vector2 = UnityEngine.Vector2;
 
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
+    
+    [Header("Movement")]
     public float MoveSpeed = 5f;
-
     private float horizontalMovement;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    [Header("Jump")]
+    private float JumpPower = 10f;
+
+   
     void Start()
     {
         
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         rb.linearVelocity = new Vector2(horizontalMovement * MoveSpeed, rb.linearVelocity.y);
@@ -22,5 +29,20 @@ public class PlayerMovement : MonoBehaviour
     public void Move(InputAction.CallbackContext context)
     {
         horizontalMovement = context.ReadValue<Vector2>().x;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            //Hold down jump button = full height
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, JumpPower);
+        }
+        
+        else if (context.canceled)
+        {
+            //Hold down jump button = half the height
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
+        }
     }
 }
