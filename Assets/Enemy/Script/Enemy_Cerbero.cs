afcab7e415f06c43ba1b38a79e86a94fac8a8b6c
wowyesol <bmo11152598@gmail.com>
2025-04-22 18:26:49 +0900

    0422 Cebero attack

--- Assets/Enemy/Script/Enemy_Cerbero.cs
+++ Assets/Enemy/Script/Enemy_Cerbero.cs
@@ -11,6 +11,10 @@ public class Enemy_Cerbero : MonoBehaviour
     private SpriteRenderer spriteRenderer;
     public bool canMove = true;
     public bool inRange = false;
+    
+    public Transform attackPoint;
+    public float attackRange = 1f;
+    public LayerMask playerLayer;
 
     void Awake()
     {
@@ -27,6 +31,9 @@ public class Enemy_Cerbero : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Cer_Attack"))
+            return;
+        
         if (player.position.x < transform.position.x)
             spriteRenderer.flipX = true;  
         else
@@ -63,7 +70,20 @@ public class Enemy_Cerbero : MonoBehaviour
             yield return new WaitForSeconds(1.5f);
             
             animator.SetTrigger("Attack");
+            
             canMove = true;
+        }
+    }
+    
+    public void DealDamageToPlayer()
+    {
+        Debug.Log("cerbero 공격 이벤트");
+        Collider2D hit = Physics2D.OverlapCircle(attackPoint.position, attackRange, playerLayer);
+
+        if (hit != null && hit.CompareTag("Player"))
+        {
+            hit.GetComponent<PlayerHealth>()?.TakeDamage(1);
+            Debug.Log("cerbero 데미지");
         }
     }
 }
