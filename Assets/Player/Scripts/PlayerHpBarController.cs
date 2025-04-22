15c0080d3eecb1fbb40cf587578a02e1cff9f6c8
9ot9am <9ot9am@gmail.com>
2025-04-22 18:11:59 +0900

    feat(player):healthbar

---
+++ Assets/Player/Scripts/PlayerHpBarController.cs
@@ -0,0 +1,27 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHpBarController : MonoBehaviour
+{
+    public Slider healthSlider;
+    public TextMeshProUGUI healthText;
+
+    public void SetMaxHealth(int max)
+    {
+        healthSlider.maxValue = max;
+        UpdateText(max, max);
+       
+    }
+
+    public void SetCurrentHealth(int current)
+    {
+        healthSlider.value = current;
+        UpdateText(current, (int)healthSlider.maxValue);
+    }
+
+    private void UpdateText(int current, int max)
+    {
+        healthText.text = $" {current} / {max}";
+    }
+}
